b43502b4be7a23ea970078da360bad8b4cd91452
yghaddar <yaghaddar@outlook.com>
2020-04-23 17:38:13 -0400

    worked on grey boz wireframe and css

--- app.js
+++ app.js
@@ -5,36 +5,63 @@ const store = {
     // 5 or more questions are required
     questions: [
       {
-        question: 'What color is broccoli?',
+        question: 'The Atari 2600 was released in the United States in which year?',
         answers: [
-          'red',
-          'orange',
-          'pink',
-          'green'
+          '1977',
+          '1980',
+          '2001',
+          '1600'
         ],
-        correctAnswer: 'green'
+        correctAnswer: '1977'
       },
       {
-        question: 'What is the current year?',
+        question: 'What is the name of the main protagonist of the Tomb Raider series of games?',
         answers: [
-          '1970',
-          '2015',
-          '2019',
-          '2005'
+          'Michelle Obama',
+          'Diana Prince',
+          'Lara Croft',
+          'Yuna'
         ],
-        correctAnswer: '2019'
+        correctAnswer: 'Lara Croft'
+      }
+        question: ' Mario first appeared in which classic video game?',
+        answers: [
+          'Mario Brothers',
+          'Donkey Kong',
+          'Super Mario',
+          'The legend of Mario'
+        ],
+        correctAnswer: 'Donkey Kong'
+      {
+        question: 'Which company publishes World of Warcraft?',
+        answers: [
+          'Respawn',
+          'Thinkful Co',
+          'Saga Inc',
+          'Blizzard Inc'
+        ],
+        correctAnswer: 'Blizzard Inc'
+      }
+        question: 'The kingdom of Hyrule is the main setting for which classic video game franchise?',
+        answers: [
+          'Gears of War',
+          'Final Fantasy',
+          'The Legend of Zelda',
+          'Grand Theft Auto'
+        ],
+        correctAnswer: 'The Legend of Zelda'
       }
     ],
     quizStarted: false,
     questionNumber: 0,
     score: 0
   };
-  
+
   /**
-   * 
+   *
    * Technical requirements:
-   * 
-   * Your app should include a render() function, that regenerates the view each time the store is updated. 
+   *
+   * Your app should include a render() function, that regenerates the view each time the store is updated.
    * See your course material, consult your instructor, and reference the slides for more details.
    *
    * NO additional HTML elements should be added to the index.html file.
@@ -42,17 +69,67 @@ const store = {
    * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
    *
    * SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING 👇
-   * 
+   *
    */
-  
+
   /********** TEMPLATE GENERATION FUNCTIONS **********/
-  
+
+function startQuizTemplate(){
+$('main').html(`<section class="startPage">
+<h1>Video Game History Quiz</h1>
+<h2>Think you know your video games?</h2>
+<button class="startButton">Let's go!</button>
+</section>`)
+}
+
+function questionTemplate(){
+
+}
+
+function incorrectResultTemplate(){
+
+}
+
+function correctResultTemplate(){
+
+}
+
+function endQuizTemplate() {
+
+}
   // These functions return HTML templates
-  
+
   /********** RENDER FUNCTION(S) **********/
-  
+
+  function renderQuestions(){
+
+  }
+
+
+
   // This function conditionally replaces the contents of the <main> tag based on the state of the store
-  
+
   /********** EVENT HANDLER FUNCTIONS **********/
-  
+
   // These functions handle events (submit, click, etc)
+function startGameHandle(){
+
+}
+
+function enterAnswerHandle(){
+
+}
+
+function nextQuestionHandle(){
+  
+}
+
+function restartGamehandle(){
+
+}
+
+function handleQuizApp(){
+
+}
+
+$(handleQuizApp)
